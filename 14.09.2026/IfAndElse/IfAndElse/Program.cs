e7c489fce23ced4c719a25204068a81a67c92404
Opilane Techno+TLN <illimar.sulbi@gmail.com>
2026-09-14 09:58:13 +0300

    IfAndElse

---
+++ 14.09.2026/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Illimar sulbi");
+
+            //siin on muutuja nimega name,
+            //mis on tüübiga string
+            //loeb andmeid konsoolist ja salvestab
+            //need muutuja name sisse
+            string name = Console.ReadLine();
+            //if ja else kontrollib, kas muutuja
+            //name on tühi või mitte
+            //kui teksti "tere, TUndmatu! error" ja teeeb paar piikus
+
+            if (name != "")
+            {
+                Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("tere, Tundmatu Error");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+
+
+            }
+
+        }
+    }
+}
