f9acf343b23d42dcc1ddcc998274001f8aaa66e9
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-21 10:01:04 +0300

    IfAndElse 2

---
+++ 03.21.09.2026/IfAndElseNesting/IfAndElseNesting/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if ja else nestimine iseseivalt");
+            Console.WriteLine("Teise else if-i sisse panna if ja else");
+            double x = 1;
+
+            if (x == 2)
+            {
+
+
+            }
+            else if (x == 4)
+            {
+                Console.WriteLine("Vasuts on 4");
+
+                if (x == 3)
+                {
+                    Console.WriteLine("vatus on 3");
+                }
+                else { Console.WriteLine("vastus ei ole 3"); }
+            }
+            else if (x == 5) ;
+
+            else
+
+                Console.WriteLine("mingi kahtlane number");
+        }
+
+    }
+}
+    
+
