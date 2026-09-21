4b92f7f3a2262cb4e89d0b14f11c1ab0984b33ab
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-21 11:00:05 +0300

    IfAndElseColours

---
+++ 03.21.09.2026/IfAndElseColours/IfAndElseColours/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace IfAndElseColours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Teha if ja else konsoolirakendus, kus kontrollitakse stringi abil värvi vastavust");
+            Console.WriteLine("Värvide valikus on: red, blue, green ja white");
+            Console.WriteLine("Peab käsitlema juhust, kus vastaja ei sisesta eelpool sisestatud värvi");
+
+            Console.WriteLine("Sisesta värv");
+            string input = Console.ReadLine();
+
+
+            if (input == "red")
+            {
+                Console.WriteLine("Valisid värvi red");
+            }
+            else if (input == "blue")
+            {
+                Console.WriteLine("Valisid värvi blue");
+            }
+            else if (input == "Green")
+            {
+                Console.WriteLine("Valisid värvi Green");
+            }
+            else if (input == "White")
+            {
+                Console.WriteLine("Valisid värvi White");
+            }
+            else
+            {
+
+                Console.WriteLine("Tundmatu värv");
+            }
+        }
+    }
+}
